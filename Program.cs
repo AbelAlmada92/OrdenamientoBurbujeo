4438914e04569da6145d1e36d8cee7f48c874cfe
Abel Almada <abel.almada.1992@gmail.com>
2023-02-07 20:24:44 -0600

    Ordenamiento Burbujeo commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OrdenamientoBurbujeo
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int i = 0;
+            int k = 0;
+            int CAN = 0;
+            int AUX = 0;
+            string linea;
+            Console.WriteLine("Cuantos elementos MAX=12. ");
+            linea = Console.ReadLine();
+            CAN = int.Parse(linea);
+            int[] VEC = new int [CAN + 1];
+
+            for(i = 1; i <= CAN; i++)
+            {
+                Console.WriteLine("Posicion {0} ==> ", i);
+                linea = Console.ReadLine();
+                VEC[i] = int.Parse(linea);
+            }
+
+            for (i = 1; i <= CAN; i++)
+            {
+               for(k = i + 1; k <= CAN; k++)
+               {
+                if((VEC[i] < VEC[k] ))
+                {
+                    AUX = VEC[k];
+                    VEC [k] = VEC [i];
+                    VEC [i] = AUX;
+                }
+               }
+            }
+          
+            Console.WriteLine();
+            Console.WriteLine("Arreglo ordenado descendete ");
+             for(i = 1; i <= CAN; i++)
+            {
+                Console.WriteLine(VEC[i]);
+            }
+            Console.WriteLine("Pulse una tecla.");
+            Console.ReadLine();
+        }
+    }
+}
